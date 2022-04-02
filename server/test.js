be1e9b7f8e57ff3514053e307b2009898b727ada
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-04-02 11:55:17 +0600

    data save in database using test.js file

--- server/test.js
+++ server/test.js
@@ -5,9 +5,25 @@ mongoose
     serverSelectionTimeoutMS: 1000,
   })
 
-  .then(() => {
+  .then(async () => {
     console.log("database conntected");
+    await createUser({ name: "Alamin sheikh", email: "alamin@gmail.com" });
+    await createUser({ name: "Alamin sheikh1", email: "alamin1@gmail.com" });
+    mongoose.connection.close(true);
   })
   .catch((e) => {
     console.log(e);
   });
+
+const Schema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", Schema);
+
+async function createUser(data) {
+  const user = new User({ ...data });
+  await user.save();
+  return user;
+}
