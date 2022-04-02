68c0a9ce934f39af757eb77fbaed0eaf298792bb
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-04-02 23:42:11 +0600

    use app.use

--- server/server.js
+++ server/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const connectDB = require("./db");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "invalid data" });
+  }
+});
+
+app.get("/", (_, res) => {
   const obj = {
     name: "Alamin",
     email: "alamin@gmail.com",
