bacd09ec7794fa829a05ff8cf36b61d2bd948557
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-04-03 21:58:48 +0600

    email and name validation from user model

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,18 @@ const { model, Schema } = require("mongoose");
 const userSchema = new Schema({
   name: {
     type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 25,
   },
   email: {
     type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+    },
   },
   password: {
     type: String,
