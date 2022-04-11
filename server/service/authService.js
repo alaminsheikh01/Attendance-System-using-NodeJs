5ba94d13aa8a564377a539e9298de97a944e876f
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-04-11 21:15:48 +0600

    register and login controller structured

--- server/service/authService.js
+++ server/service/authService.js
@@ -1,38 +1,45 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { findUserByProperty, createNewUser } = require("./userService");
 
-const registerService = async () => {
-  let user = await User.findOne({ email });
+const registerService = async ({ name, email, password }) => {
+  let user = await findUserByProperty("email", email);
   if (user) {
-    return res.status(400).json({ message: "user already exist" });
+    // return res.status(400).json({ message: "user already exist" });
+    const error = new Error("User already exist");
+    error.status = 400;
+    throw error;
   }
 
   user = new User({ name, email, password });
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
-  user.password = hash;
-
-  await user.save();
+  createNewUser({ name, email, password: hash });
 };
 
-const loginService = async () => {
-  const user = await User.findOne({ email });
-
+const loginService = async ({ email, password }) => {
+  const user = await findUserByProperty("email", email);
   if (!user) {
-    return res.status(400).json({ messae: "Invalid Credential" });
+    const error = new Error("Invalid Credential");
+    error.status = 400;
+    throw error;
   }
-
   const isMatch = await bcrypt.compare(password, user.password);
-
   if (!isMatch) {
-    return res.status(400).json({ messaeg: "Invalid Credential" });
+    const error = new Error("Invalid Credential");
+    error.status = 400;
+    throw error;
   }
-
-  delete user._doc.password;
-
-  const token = jwt.sign(user._doc, "secret-key", { expiresIn: "1h" });
+  const payload = {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    roles: user.roles,
+    accountStatus: user.accountStatus,
+  };
+  return jwt.sign(payload, "secret-key", { expiresIn: "1h" });
 };
 
 module.exports = {
