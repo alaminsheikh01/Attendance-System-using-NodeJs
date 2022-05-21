e64635449e5b844da32c44920fb7f03941fa8c63
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-05-21 12:26:59 +0600

    bug fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
-const private = require("../middleware/authenticate");
+const authentic = require("../middleware/authenticate");
 
 router.use("/api/v1/auth", authRoutes);
-router.use("/api/v1/users", private, userRoutes);
+router.use("/api/v1/users", authentic, userRoutes);
 
 module.exports = router;
