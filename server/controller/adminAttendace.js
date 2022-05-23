e3810545b41c053d8046e71641844e836085afb6
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-05-23 16:48:12 +0600

    updated

--- server/controller/adminAttendace.js
+++ server/controller/adminAttendace.js
@@ -1,8 +1,39 @@
-const getEnable = (req, res, next) => {};
+const {} = require("date-fns");
+const AdminAttendance = require("../models/AdminAttendance");
+const error = require("../utils/error");
+
+const getEnable = async (req, res, next) => {
+  try {
+    const running = await AdminAttendance.findOne({ status: "RUNNING" });
+    if (running) {
+      throw error("Already Running", 400);
+    }
+
+    const attendance = new AdminAttendance({});
+    await attendance.save();
+
+    return res.status(201).json({ messae: "Success", attendance });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const getStatus = async (req, res, next) => {
+  try {
+    const running = await AdminAttendance.findOne({ status: "RUNNING" });
+    if (!running) {
+      throw error("Not Running", 400);
+    }
+    return res.status(200).json(running);
+  } catch (e) {
+    next(e);
+  }
+};
 
 const getDisable = (req, res, next) => {};
 
 module.exports = {
   getEnable,
   getDisable,
+  getStatus,
 };
--- server/routes/admin-attendance-route.js
+++ server/routes/admin-attendance-route.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { getDisable, getEnable } = require("../controller/adminAttendace");
+const {
+  getDisable,
+  getEnable,
+  getStatus,
+} = require("../controller/adminAttendace");
 
 router.get("/enable", getEnable);
 router.get("/disable", getDisable);
+router.get("/status", getStatus);
 
 module.exports = router;
